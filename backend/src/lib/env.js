d0734cdc9319bed10a5206ce4f1d8ed29f052aec
Vinod Bhanji <bhanjivinod11@gmail.com>
2026-02-05 16:35:01 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -3,5 +3,6 @@ dotenv.config()
 
 export const ENV={
     PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL
+    DB_URL: process.env.DB_URL,
+    IS_PRODUCTION: process.env.IS_PRODUCTION
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,30 @@
-import express from 'express';
-import {ENV} from  './lib/env.js'
+import express from 'express'
+import { ENV } from './lib/env.js'
+import path from 'path'
 
-const app = express();
+const app = express()
+const __dirname = path.resolve()
 
-app.get('/', (req, res)=>{
-    res.status(200).json({msg:'Success from API__'})
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'Success from API__' })
 })
 
-app.listen(ENV.PORT, ()=>{
-    console.log("Running on the POST NO: 5000")
+app.get('/books', (req, res) => {
+  res.status(200).json({ msg: 'Success from BOOKS section' })
+})
+
+// bringing front end and backend code in the same url 5000 port for production purposeg
+if (ENV.IS_PRODUCTION === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+  // SPA fallback — Express 5 safe
+  app.use('/{*any}',(req, res) => {
+    res.sendFile(
+      path.join(__dirname, '../frontend/dist/index.html')
+    )
+  })
+}
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server running on port ${ENV.PORT}`)
 })
