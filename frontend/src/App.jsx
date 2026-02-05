e4e5ec495c697c581f3c62a7ada400c29bb2969d
Vinod Bhanji <bhanjivinod11@gmail.com>
2026-02-05 18:33:05 +0530

    beck front conn test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL)
+      .then(res => res.json())
+      .then(data => {
+        console.log("Backend Response:", data)
+        setData(data.msg)
+      })
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Frontend Connected Test</h1>
+      <h2>{data ? data : "Loading from backend..."}</h2>
     </>
   )
 }
