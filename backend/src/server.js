8409b1c1a2fe8ba4827aa37d84a2c52afe27bcb4
Vinod Bhanji <bhanjivinod11@gmail.com>
2026-02-07 16:16:14 +0530

    inngest3

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import serve from "@inngest/express";
+import serve from "inngest/express";
 import { functions } from "./models/ingest.js";
 import {connDB} from './lib/db.js'
 import { inngest } from "../models/ingest.js";
